17c9b5f36a50659a4c5f689f921c329bec7f6cea
Sushant0907 <gangurdesushant847@gmail.com>
2023-07-16 17:24:49 +0530

    added *

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 5000;                 
 
 
-app.get('/', (req, res) => {        
+app.get('/*', (req, res) => {        
     res.sendFile('index.html', {root: __dirname});      
                                                         
 });
